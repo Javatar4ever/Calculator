e37f278a8ee969f163abcb96903cd04ffe2b5871
Javatar4ever <teo.ruokolainen@outlook.com>
2016-11-11 14:04:51 +0100

    - Added more buttons to GUI - Implemented memory functions - Added pi - Added functions: Backspace, Clear Everything and Clear - Added functionality to handle long equations - Added parenthesises - Added functionality to log window

--- src/sample/Calculate.java
+++ src/sample/Calculate.java
@@ -2,9 +2,6 @@ package sample;
 
 import java.util.*;
 
-/**
- * Created by tedda on 07/11/2016.
- */
 public class Calculate {
 
     interface Operation {
@@ -20,19 +17,22 @@ public class Calculate {
         operations.put("/", (a, b) -> a / b);
     }
 
+    public Calculate() {
+        System.out.println(getRPN("10*(10+9)-5"));
+    }
+
     public double getAnswer(String equation) {
 
         String rpn = getRPN(equation);
-        double result = solveRPN(rpn);
-
-        return result;
+        return solveRPN(rpn);
     }
 
     /*Shunting-yard algorithm
     Returns equation in Reverse Polish Notation (RPN)
 
-    + - : priority 0
-    / * : priority 1
+    ( ) : priority 0
+    + - : priority 1
+    / * : priority 2
 
     */
     private String getRPN(String equation) {
@@ -41,38 +41,56 @@ public class Calculate {
         Stack<String> stack = new Stack<>();
         int priority = 0;
 
-        String[] tokens = equation.split("(?<=[-+*/])|(?=[-+*/])");
+        String[] tokens = equation.split("(?<=[-+*/()])|(?=[-+*/()])");
 
         for (int i = 0; i < tokens.length; i++) {
-            if (tokens[i].matches("[-+*/]")) { //use of regular expression (regex) to see if operator
+            if (tokens[i].matches("[-+*/()]")) { //use of regular expression (regex) to see if operator
 
                 if (stack.isEmpty()) {
                     stack.push(tokens[i]);
                 } else {
 
-                    boolean loop = true;
+                    //Handle parantheses
 
-                        String topToken = stack.peek(); //peek at stack to get it's priority
+                    if (tokens[i].matches("[()]")) {
+                        if (tokens[i].equals("(")) {
+                            stack.push(tokens[i]);
 
-                        if (topToken.matches("[*/]")) priority = 1;
-                        else if (topToken.matches("[+-]")) priority = 0;
-
-                        if (priority == 1) {
-                            if (tokens[i].matches("[+-]")) {
-                                rpn += stack.pop() + " "; //adds to output because priority is higher than + and -
-                                i--; //makes so the same priority check can be done on the next operator in stack
-                            } else if (tokens[i].matches("[*/]")){
-                                rpn += stack.pop() + " "; //adds to output because priority is equal to / and *
-                                i--;
-                            }
-                        } else if (priority == 0) {
-                            if (tokens[i].matches("[+-]")) {
+                        } else {
+                            while (!stack.isEmpty() && !stack.peek().equals("(")) {
                                 rpn += stack.pop() + " ";
-                                i--;
-                            } else if (tokens[i].matches("[*/]")) {
-                                stack.push(tokens[i]); //pushes because * and / has higher priority than 0
                             }
+
+                            stack.pop(); //disregard left paranthesis
                         }
+                        continue;
+                    }
+
+
+                    String topToken = stack.peek(); //peek at stack to get it's priority
+
+                    if (topToken.matches("[*/]")) priority = 2;
+                    else if (topToken.matches("[+-]")) priority = 1;
+                    else if (topToken.matches("[(]")) priority = 0;
+
+                    if (priority == 2) {
+                        if (tokens[i].matches("[+-]")) {
+                            rpn += stack.pop() + " "; //adds to output because priority is higher than + and -
+                            i--; //makes so the same priority check can be done on the next operator in stack
+                        } else if (tokens[i].matches("[*/]")){
+                            rpn += stack.pop() + " "; //adds to output because priority is equal to / and *
+                            i--;
+                        }
+                    } else if (priority == 1) {
+                        if (tokens[i].matches("[+-]")) {
+                            rpn += stack.pop() + " ";
+                            i--;
+                        } else if (tokens[i].matches("[*/]")) {
+                            stack.push(tokens[i]); //pushes because * and / has higher priority than 0
+                        }
+                    } else { //condition == 0
+                        stack.push(tokens[i]); //paranthesis can't be added to output queue
+                    }
                 }
 
             } else { //token is a number
@@ -95,18 +113,18 @@ public class Calculate {
 
         String[] tokens = rpn.split(" ");
 
-        for (int i = 0; i < tokens.length; i++) {
-            if (tokens[i].matches("[-+*/]")) {
+        for (String token : tokens) {
+            if (token.matches("[-+*/]")) {
 
                 rightOperand = Double.parseDouble(stack.pop());
                 leftOperand = Double.parseDouble(stack.pop());
 
-                double result = operations.get(tokens[i]).calculate(leftOperand, rightOperand);
+                double result = operations.get(token).calculate(leftOperand, rightOperand);
 
                 stack.push(Double.toString(result));
 
             } else {
-                stack.push(tokens[i]);
+                stack.push(token);
             }
         }
 
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,6 +6,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,10 +18,13 @@ public class Controller {
     private Button one, two, three, four, five, six, seven, eight, nine, zero, dot, equals, subtract, add, divide, multiply;
     @FXML
     private Label display, historydisplay;
+    @FXML
+    private TextArea logTextArea;
 
     private String displayText = "", historydisplayText = "";
 
     private Calculate calculate;
+    private double memory;
 
 
 
@@ -33,34 +37,25 @@ public class Controller {
     public void numPressed(ActionEvent e) {
 
         if(e.getSource() == zero) { //makes sure the user cannot enter zeroes before other numbers
-            if(!displayText.equals("0") && !displayText.equals("")) {
-                displayText += "0";
-                display.setText(displayText);
-            }
+            if(!displayText.equals("0") && !displayText.equals(""))
+                setDisplayText(displayText += "0");
         } else if (e.getSource() == dot) {
-            if (displayText.equals("")) {
-                displayText = "0.";
-                display.setText(displayText);
-            }
-            if (!displayText.contains(".")) {
-                displayText += ".";
-                display.setText(displayText);
-            }
-        } else {
-            displayText += (((Button)e.getSource()).getText());
-            display.setText(displayText);
-        }
+            if (displayText.equals(""))
+                setDisplayText("0.");
+            if (!displayText.contains("."))
+                setDisplayText(displayText += ".");
+        } else
+            setDisplayText(displayText += (((Button)e.getSource()).getText()));
 
     }
 
     public void operationPressed(ActionEvent e) {
 
-        if (!displayText.equals("")) {
+        if (!displayText.equals("") || historydisplayText.endsWith(")")) {
             historydisplayText += displayText + ((Button) e.getSource()).getText();
             historydisplay.setText(historydisplayText);
 
-            displayText = "";
-            display.setText(displayText);
+            setDisplayText("");
         }
     }
 
@@ -79,9 +74,116 @@ public class Controller {
             else
                 display.setText(result + "");
 
+            //Write to log window
+            logTextArea.appendText(historydisplayText + " = " + result + "\n");
+
+
             historydisplayText = "";
             displayText = "";
         }
 
     }
+
+    public void memoryPressed(ActionEvent e) {
+        switch (((Button)e.getSource()).getText()) {
+
+            case ("MS"): //Memory Store
+                memory = Double.parseDouble(displayText);
+                break;
+            case ("M+"): //Memory Add
+                memory += Double.parseDouble(displayText);
+                break;
+            case ("MC"): //Memory Clear
+                memory = 0;
+                break;
+            case ("MR"): //Memory Recall
+                if (memory % 1 == 0) {
+                    setDisplayText(Integer.toString((int)memory));
+                } else {
+                    setDisplayText(Double.toString(memory));
+                }
+                break;
+        }
+    }
+
+    public void controlPressed(ActionEvent e) {
+
+        switch (((Button)e.getSource()).getText()) {
+
+            case ("<--"):
+                if (displayText.length() != 0)
+                    setDisplayText(displayText.substring(0, displayText.length() - 1));
+                break;
+            case ("CE"):
+                historydisplayText = "";
+                historydisplay.setText(historydisplayText);
+                setDisplayText("");
+                break;
+            case ("C"):
+                setDisplayText("");
+                break;
+        }
+
+    }
+
+    public void advancePressed(ActionEvent e) {
+        switch (((Button)e.getSource()).getText()) {
+
+            case ("x^2"):
+
+                break;
+            case ("x^3"):
+
+                break;
+            case ("x^y"):
+
+                break;
+            case ("sin"):
+
+                break;
+            case ("cos"):
+
+                break;
+            case ("tan"):
+
+                break;
+            case ("sqrt"):
+
+                break;
+            case ("%"):
+
+                break;
+            case ("bin"):
+
+                break;
+            case ("("):
+                historydisplayText += "(";
+                historydisplay.setText(historydisplayText);
+                displayText = "";
+                break;
+            case (")"):
+                historydisplayText += displayText + ")";
+                historydisplay.setText(historydisplayText);
+                setDisplayText("");
+                break;
+            case ("π"):
+                setDisplayText(Double.toString(Math.PI));
+                break;
+        }
+    }
+
+
+    private void setDisplayText(String text) {
+        if (text.equals("")) {
+            displayText = text;
+            display.setText("0");
+        } else {
+            displayText = text;
+            display.setText(displayText);
+        }
+
+
+    }
+
+
 }
